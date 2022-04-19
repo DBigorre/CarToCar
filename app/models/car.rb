42097f7e9326253261d17d5085d3209395f89596
desiree bigorre <bigorredesiree@gmail.com>
2022-04-19 14:54:58 -0400

    creation des deux modeles

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
+
+  validates :brand, :model, :year_of_production, :adress, presence: true
+  validates :price_per_day, numericality: { greater_than: 0 }
+  
 end
