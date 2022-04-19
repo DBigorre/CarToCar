5766711b3490990182cb5d76e522109640853bd8
desiree bigorre <bigorredesiree@gmail.com>
2022-04-19 15:33:02 -0400

    creation des validates et dependant

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
 
   validates :brand, :model, :year_of_production, :adress, presence: true
   validates :price_per_day, numericality: { greater_than: 0 }
-  
+
 end
